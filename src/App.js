c9e359efc130dde01732ce3a3f082f47f6410095
Jakub Nowicki <kuba@appsilon.com>
2020-02-18 09:07:00 +0100

    basic category label

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import InputNumeric from './components/input.js';
+import CategoryLabel from './components/category_label.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <CategoryLabel/>
       <InputNumeric/>
     </div>
   );
---
+++ src/components/category_label.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoryLabel = () => {
+  return <div>Category</div>;
+};
+
+export default CategoryLabel;
