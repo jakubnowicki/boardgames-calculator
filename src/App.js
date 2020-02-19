e8da6a757ce86da3cb60b44dc5318ac68ac16d7b
Jakub Nowicki <kuba@appsilon.com>
2020-02-19 09:11:06 +0100

    player name init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import InputNumeric from './components/input.js';
 import CategoryLabel from './components/category_label.js'
+import PlayerName from './components/player_name.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <PlayerName/>
       <CategoryLabel/>
       <InputNumeric/>
     </div>
---
+++ src/components/player_name.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayerName = () => {
+  return <div>Player Name</div>;
+};
+
+export default PlayerName;
