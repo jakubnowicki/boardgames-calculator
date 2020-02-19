1d8fc4707c9323db494a9e61c1a500837406758f
Jakub Nowicki <kuba@appsilon.com>
2020-02-19 09:14:26 +0100

    player score init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InputNumeric from './components/input.js';
-import CategoryLabel from './components/category_label.js'
-import PlayerName from './components/player_name.js'
+import CategoryLabel from './components/category_label.js';
+import PlayerName from './components/player_name.js';
+import PlayerScore from './components/player_score.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <PlayerName/>
+      <PlayerScore/>
       <CategoryLabel/>
       <InputNumeric/>
     </div>
---
+++ src/components/player_score.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayerScore = () => {
+  return <div>0</div>;
+};
+
+export default PlayerScore;
