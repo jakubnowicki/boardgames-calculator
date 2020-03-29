0ff8f7dfa38afe1325a4313ba1726dca2f2f12fc
Jakub Nowicki <kuba@appsilon.com>
2020-03-29 13:01:27 +0200

    init header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Row } from "react-grid-system";
+
+const Header = () => {
+  return (
+    <Row>
+      <div className="header">Header</div>
+    </Row>
+  );
+};
+
+export default Header;
